ef8e1ca8febf157315ab1201cdffd2d6892632fd
Michael Douglas <michaeldouglas@Shandas-Mac-mini.local>
2021-01-03 14:52:26 -0700

    adding react context, wrapping app in provider

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { ColumnContainer, ColumnTitle } from './styles';
 import { AddNewItem } from './AddNewItem';
-
+import { Card } from './Card';
+import { useAppState } from './AppStateContext';
 interface ColumnProps {
-    text: string
+    text: string,
+    index: number
 };
 
 export const Column = ({
     text,
-    children
-}: React.PropsWithChildren<ColumnProps>) => {
+    index
+}:ColumnProps) => {
+    const { state } = useAppState();
     return (
         <ColumnContainer>
-            <ColumnTitle>{text}</ColumnTitle>
-            {children}
-            <AddNewItem
+           <ColumnTitle>{text}</ColumnTitle>
+           {state.lists[index].tasks.map(task => (
+               <Card text={task.text} key={task.id}/>
+           ))}
+           <AddNewItem
                 toggleButtonText="+ Add another task"
                 onAdd={console.log}
                 dark
